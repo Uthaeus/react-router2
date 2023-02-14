16a47e48d5650f185911ec1494c59f8a1021eb80
Uthaeus <romanlavery@gmail.com>
2023-02-14 10:44:13 -0700

    installed react router and created components for different pages

---
+++ src/pages/EditEvent.js
@@ -0,0 +1,11 @@
+
+
+const EditEventPage = () => {
+
+    return (
+        <h1>Edit Event</h1>
+    );
+};
+
+
+export default EditEventPage;
---
+++ src/pages/Events.js
@@ -0,0 +1,11 @@
+
+
+const EventsPage = () => {
+
+    return (
+        <h1>Events Page</h1>
+    );
+};
+
+
+export default EventsPage;
