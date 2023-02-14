242071fff0b9c3e0079d4145efcc80ed81f36711
Uthaeus <romanlavery@gmail.com>
2023-02-14 11:00:13 -0700

    created Layout component and set up routing to different pages

--- src/App.js
+++ src/App.js
@@ -20,8 +20,31 @@
 // 7. Output the ID of the selected event on the EventDetailPage
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
+import { createBrowserRouter, RouterProvider} from 'react-router-dom';
+
+import EditEventPage from './pages/EditEvent';
+import EventDetailPage from './pages/EventDetail';
+import EventsPage from './pages/Events';
+import HomePage from './pages/Home';
+import Layout from './pages/Layout';
+import NewEventPage from './pages/NewEvent';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: '/events', element: <EventsPage /> },
+      { path: '/events/:eventId', element: <EventDetailPage /> },
+      { path: '/events/:eventId/edit', element: <EditEventPage /> },
+      { path: '/events/new', element: <NewEventPage /> },
+    ]
+  }
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/Layout.js
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom";
+
+import MainNavigation from '../components/MainNavigation';
+
+const Layout = () => {
+
+    return (
+        <>
+            <MainNavigation />
+            <main>
+                <Outlet />
+            </main>
+        </>
+    );
+};
+
+
+export default Layout;
